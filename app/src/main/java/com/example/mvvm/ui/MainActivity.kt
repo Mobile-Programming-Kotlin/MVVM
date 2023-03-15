b6cb612bc90ec9d0c7b6bee65e2b2b0b91a5cfe5
WellyBelly <89990168+WellyBelly@users.noreply.github.com>
2023-03-15 16:42:23 +0200

    Finished the first part

--- app/src/main/java/com/example/mvvm/ui/MainActivity.kt
+++ app/src/main/java/com/example/mvvm/ui/MainActivity.kt
@@ -1,16 +1,24 @@
-package com.example.mvvm
+package com.example.mvvm.ui
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Divider
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.mvvm.ui.theme.MVVMTheme
+import com.example.mvvm.viewmodel.TodoViewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,8 +38,23 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun MyApp() {
-    Text(text = "Hello $name!")
+fun MyApp(todoViewModel: TodoViewModel = viewModel()) {
+    TodoList(todoViewModel.todos)
+}
+
+@Composable
+fun TodoList(todos: List<String>) {
+    LazyColumn(
+        modifier = Modifier.padding(8.dp)
+    ){
+        items(todos) { todo ->
+            Text(
+                text = todo,
+                modifier = Modifier.padding(top = 4.dp, bottom = 4.dp)
+            )
+            Divider(color = Color.LightGray, thickness = 1.dp)
+        }
+    }
 }
 
 @Preview(showBackground = true)
